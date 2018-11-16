4f8db70b452cfde42a28af4dda0d8de65d36a7b1
volkan@clouddemo01.com <volkanv@au1.ibm.com>
2018-11-16 13:24:01 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const hbs = require('hbs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials/');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+})
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+})
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return  new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: "Bad route"
+    });
+});
+
+app.listen(3000);
